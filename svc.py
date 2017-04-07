38c05871aa4d26c81c2b6c7195bf48cf79922a68
Nelson Salinas <nrsalinas@gmail.com>
2017-04-07 17:31:46 -0400

    SVM.

---
+++ svc.py
@@ -0,0 +1,28 @@
+import readsim
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
+bffr = ''
+X, Y = readsim.get_arrays(kmer_size = 2, classes = 30, sim_seq_length = 33, reads_per_domain = 10, n_jobs = 4)
+
+##############################################
+# Grid search GradientBoostingClassifier()
+##############################################
+
+param2test = {"C" : [0.1, 1.0, 10.0],
+	"kernel" : ["rbf", "poly", "sigmoid"],
+	"gamma" = [0.01, 0.1, 1.0],
+	"coef0" = [0.0, 5.0, 10.0],
+	"degree" = [2, 3, 5]
+	}
+
+model = SVC()
+grid_search = GridSearchCV(estimator = model, param_grid = param2test, cv = 10)
+bffr += "Training matrix shape: {0}\n".format(X.shape)
+bffr += "Shape of training label vector: {0}\n".format(Y.shape)
+grid_search.fit(X, Y)
+
+bffr += "Best score: {0}\n".format(grid_search.best_score_)
+bffr += "Best parameter set: {0}\n".format(grid_search.best_params_)
+bffr += "\n\nResults DataFrame:\n{0}\n".format(grid_search.cv_results_)
+print bffr
