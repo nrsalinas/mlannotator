598ed6156b2ed0496a6b1ba39d624314414c297e
Nelson Salinas <nrsalinas@gmail.com>
2017-04-20 18:45:25 -0400

    Short scoring.

--- svc.py
+++ svc.py
@@ -3,23 +3,23 @@ from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 
 bffr = ''
-X, Y = readsim.get_arrays(kmer_size = 2, classes = 30, sim_seq_length = 33, reads_per_domain = 10)
+X, Y = readsim.get_arrays(kmer_size = 2, classes = 5, sim_seq_length = 33, reads_per_domain = 1)
 
 ##############################################
 # Grid search GradientBoostingClassifier()
 ##############################################
 
-param2test = {"C" : [0.1, 1.0, 10.0],
-	"kernel" : ["rbf", "poly", "sigmoid"],
-	"gamma" : [0.01, 0.1, 1.0],
-	"coef0" : [0.0, 5.0, 10.0],
+param2test = {#"C" : [0.1, 1.0, 10.0],
+#	"kernel" : ["rbf", "poly", "sigmoid"],
+#	"gamma" : [0.01, 0.1, 1.0],
+	#"coef0" : [0.0, 5.0, 10.0],
 	"degree" : [2, 3, 5]
 	}
 
 model = SVC()
-grid_search = GridSearchCV(estimator = model, param_grid = param2test, cv = 10, n_jobs = 4)
-bffr += "Training matrix shape: {0}\n".format(X.shape)
-bffr += "Shape of training label vector: {0}\n".format(Y.shape)
+grid_search = GridSearchCV(estimator = model, param_grid = param2test, verbose = True, cv = 10) #, n_jobs = 4)
+print "Training matrix shape: {0}\n".format(X.shape)
+print "Shape of training label vector: {0}\n".format(Y.shape)
 grid_search.fit(X, Y)
 
 bffr += "Best score: {0}\n".format(grid_search.best_score_)
